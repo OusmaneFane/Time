903a1bd8e84fb2866bba5edcd02a3de43dfd0bdc
OusmaneFane <ousmanefane08@gmail.com>
2022-11-04 12:31:14 +0000

    change image to laravel

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -6,9 +6,9 @@
 @section('content')
 <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
 
-<img src="/images/php.png" alt="Logo_php" class="rounded-full shadow-md mv-12">
+<img src="/images/laravel.jpeg" alt="Logo_php" class="rounded-full shadow-md mv-12">
 
-    <h2 class="text-gray-700 mb-5 text-2xl font-semibold mt-5 text-indigo-600">Fait avec <span class="text-pink-500">&hearts;</span> par Ousmane FANE.</h2>
+    <h2 class="text-gray-700 mb-5 text-2xl font-semibold mt-5 text-indigo-600">Fait avec <span class="text-pink-500">&hearts;</span> par OUSMANE FANE.</h2>
 <h2 class="text-gray-700"><a href="/" class="text-indigo-500 hover:text-indigo-600 underline">Revenir à la page d'acuueil</h2></p>
 
 <footer>
