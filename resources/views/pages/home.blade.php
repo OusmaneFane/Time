eef195828c46233396cc8558994b8351b72e423f
OusmaneFane <ousmanefane08@gmail.com>
2022-12-13 11:15:34 +0000

    .

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,7 +1,7 @@
 @extends('app')
 
 @section('title', 'Hello Bamako')
-<img src="/images/sup.png" alt="Mali" class="rounded shadow-md h-15 mt-12" >
+<img src="/images/mali.jpg" alt="Mali" class="rounded shadow-md h-15 mt-12" >
 @section('content')
 
     <h1 class="text-5xl font-semibold mt-5 text-indigo-600 ">Welcome at Sup'Management</h1>
