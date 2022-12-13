63648045cc196662c873788873af39299fa185c6
OusmaneFane <ousmanefane08@gmail.com>
2022-12-13 10:32:32 +0000

    change

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
     <title>@yield('title', 'Hello Bamako')</title>
 
 </head>
-<body class=" py-6 flex flex-col justify-between items-center min-h-screen">
+<body class=" py-6 flex flex-col justify-between items-center min-h-screen bg-slate-500">
     <main role="main" class="flex flex-col justify-center items-center">
 
         @yield('content')
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -8,7 +8,7 @@
 
 <img src="/images/laravel.jpeg" alt="Logo_php" class="rounded-full shadow-md mv-12">
 
-    <h2 class="text-gray-700 mb-5 text-2xl font-semibold mt-5 text-indigo-600">Fait avec <span class="text-pink-500">&hearts;</span> par OUSMANE FANE.</h2>
+    <h2 class="text-gray-700 mb-5 text-2xl font-semibold mt-2 text-indigo-600">Fait avec <span class="text-pink-500">&hearts;</span> par les membres du Groupe5.</h2>
 <h2 class="text-gray-700"><a href="/" class="text-indigo-500 hover:text-indigo-600 underline">Revenir à la page d'acuueil</h2></p>
 
 <footer>
